6d56b82e92193167e30aee67668795fdae6b6d16
brennanschultzy <bmschultz@mail.bradley.edu>
2019-10-07 12:19:04 -0500

    We got a Game Controller Script now

---
+++ Chef Solomon/Assets/Scripts/Game Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    GameObject beat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //beat = 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
