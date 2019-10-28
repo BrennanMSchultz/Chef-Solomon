d9391e7e8b13baf801248d3704a394aa3c37ffd4
Godofwar1300 <55067215+Godofwar1300@users.noreply.github.com>
2019-10-28 11:28:00 -0500

    Revert "Commit For script changes"
    This reverts commit bb6e7e7a5b8487c278948a5067bf53c78d557f7b, reversing
    changes made to 32abbd79708e88cad54e4a726bef0e6bfb2f218e.

--- Chef Solomon/Assets/Scripts/PlayerController.cs
+++ Chef Solomon/Assets/Scripts/PlayerController.cs
@@ -59,14 +59,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         Slice();
         //StopAtOriginalPosition();
-=======
-        LeftSlice();
-        RightSlice();
-        StopAtOriginalPosition();
->>>>>>> 7a6fa9ce4e0e200f1777a00acac7443362da81cd
 
         if (failNumber == 3)
         {
@@ -74,11 +68,10 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public void LeftSlice()
+    public void Slice()
     {
-        if (Input.GetKeyDown(KeyCode.F) && !hasPressed && this.CompareTag("Cleaver"))
+        if (Input.GetKeyDown(KeyCode.Space) && !hasPressed)
         {
-<<<<<<< HEAD
             if (Physics.Linecast(transform.position, target.position))
             {
                 print("hit");
@@ -137,61 +130,8 @@ public class PlayerController : MonoBehaviour
                     }
             */
 
-=======
-            playerRB.AddForce(Vector3.down * sliceSpeed);
-            //transform.Translate(Vector3.down * sliceSpeed * Time.deltaTime);
-            hasSliced = true;
-            hasPressed = true;
-        }
-    }
-
-    public void RightSlice()
-    {
-        if(Input.GetKeyDown(KeyCode.J) && !hasPressed && this.CompareTag("Cleaver 2"))
-        {
-            playerRB.AddForce(Vector3.down * sliceSpeed);
-            hasSliced = true;
-            hasPressed = true;
-        }
-    }
-
-    public void ReturnToPosition()
-    {
-        if (hasSliced)
-        {
-            playerRB.AddForce(originalPos * moveSpeed);
-            //playerRB.MovePosition(originalPos);
-            //playerRB.transform.Translate(originalPos);
-            //playerRB.velocity = Vector3.zero;
-            //hasPressed = false;
-        }
-    }
-
-    public void StopAtOriginalPosition()
-    {
-        if (playerRB.position.y >= originalPos.y)
-        {
-            playerRB.velocity = Vector3.zero;
-            hasPressed = false;
-        }
-    }
-
-    private void OnCollisionEnter(Collision c)
-    {
-        if (c.gameObject.CompareTag("Table"))
-        {
-            audioSource.PlayOneShot(miss);
-            audioSource.pitch -= 0.1f;
-            ReturnToPosition();
-            failNumber++;
-            noteSpeed -= 0.1f;
-            foodValue = foodController.totalScore;
-            gameManager.scoreText.text = "Score: " + foodController.SubScore(foodValue);
-            Debug.Log("Total Number of Fails is: " + failNumber);
->>>>>>> 7a6fa9ce4e0e200f1777a00acac7443362da81cd
         }
 
-<<<<<<< HEAD
         /*
             public void ReturnToPosition()
             {
@@ -219,11 +159,6 @@ public class PlayerController : MonoBehaviour
 
         /*
         private void OnCollisionEnter(Collision c)
-=======
-    private void OnTriggerEnter(Collider c)
-    {
-        if (c.gameObject.CompareTag("Food"))
->>>>>>> 7a6fa9ce4e0e200f1777a00acac7443362da81cd
         {
             if (c.gameObject.CompareTag("Table"))
             {
